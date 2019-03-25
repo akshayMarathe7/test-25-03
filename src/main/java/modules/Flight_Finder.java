af9746ff4c4eaec873ac939af4eda8c973ed5b34
akshayMarathe7 <43407370+akshayMarathe7@users.noreply.github.com>
2019-03-25 12:47:34 +0530

    Update Flight_Finder.java

--- src/main/java/modules/Flight_Finder.java
+++ src/main/java/modules/Flight_Finder.java
@@ -6,7 +6,7 @@ import testBase.TestBase;
 
 public class Flight_Finder extends TestBase
 {
-
+	// Testing pull command
 	By Type_RadioBttn = By.xpath("//input[@value='oneway']");
 	By PassengerSelection_Dropdown = By.xpath("//select[@name='passCount']/option[text()='2 ']");
 	By DepartingFrom_DropDown =By.xpath("//select[@name='fromPort']/option[@value='Paris']");
